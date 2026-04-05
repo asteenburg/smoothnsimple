01f58e317d920efefe45902a573cc637ea392fc5
asteenburg <97249534+asteenburg@users.noreply.github.com>
2026-04-05 12:45:14 -0400

    Delete components/PaymentWrapper.tsx

--- components/PaymentWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const PaymentForm = dynamic(() => import("./PaymentForm"), { ssr: false });
-
-export default function PaymentWrapper() {
-  return <PaymentForm />;
-}
