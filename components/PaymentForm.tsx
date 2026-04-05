00cc25e2b93db41a623a53a871c6167596a24026
asteenburg <97249534+asteenburg@users.noreply.github.com>
2026-04-05 12:16:15 -0400

    Add TypeScript interface for PaymentForm props
    Refactor PaymentForm component to use TypeScript interfaces for props.

--- components/PaymentForm.tsx
+++ components/PaymentForm.tsx
@@ -1,10 +1,19 @@
 "use client";
+
 import {
   CreditCard,
   PaymentForm as SquareProvider,
 } from "react-square-web-payments-sdk";
 
-export default function PaymentForm({ amount, handlePayment }: any) {
+interface PaymentFormProps {
+  amount: number;
+  handlePayment: (tokenResult: any) => void;
+}
+
+export default function PaymentForm({
+  amount,
+  handlePayment,
+}: PaymentFormProps) {
   return (
     <SquareProvider
       applicationId={process.env.NEXT_PUBLIC_SQUARE_APPLICATION_ID!}
@@ -13,8 +22,19 @@ export default function PaymentForm({ amount, handlePayment }: any) {
     >
       <CreditCard
         buttonProps={{
-          /* your styles here */
           content: `Pay $${amount}.00`,
+          style: {
+            backgroundColor: "#db2777",
+            color: "#fff",
+            borderRadius: "1.25rem",
+            padding: "20px",
+            fontWeight: "900",
+            width: "100%",
+            cursor: "pointer",
+            border: "none",
+            fontSize: "1rem",
+            textTransform: "uppercase",
+          },
         }}
       />
     </SquareProvider>
