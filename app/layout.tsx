ef73511c2fd556ac15b275652592318a5e0e0998
asteenburg <97249534+asteenburg@users.noreply.github.com>
2026-04-04 21:07:51 -0400

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import CookieBanner from "../components/cookies/CookieBanner";
-import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +24,6 @@ export default function RootLayout({
         className={`${inter.className} bg-black text-white min-h-screen flow-root`}
       >
         {children}
-        <Footer />
         <CookieBanner />
       </body>
     </html>
