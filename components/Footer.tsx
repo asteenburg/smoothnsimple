30b6eb868e94434f65a44274b5351f46aa94e4a3
asteenburg <97249534+asteenburg@users.noreply.github.com>
2026-04-07 22:06:45 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,9 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto">
 
         {/* TOP WRAPPER */}
-        <div className="flex flex-row justify-between gap-6 flex-wrap mb-16">
+        <div className="flex flex-col sm:flex-row justify-between gap-12 mb-16 flex-wrap">
 
-          {/* BRAND */}
+          {/* BRAND COLUMN */}
           <div className="w-full sm:w-auto mb-6 sm:mb-0">
             <Link href="/" className="flex items-center gap-2 mb-4 group">
               <div className="bg-pink-600 p-1 rounded-lg transition-transform group-hover:rotate-12">
@@ -26,11 +26,11 @@ export default function Footer() {
             </p>
           </div>
 
-          {/* SOCIAL + LEGAL COLUMNS */}
-          <div className="flex flex-row justify-between w-full sm:w-auto gap-6 flex-wrap">
+          {/* SOCIAL + LEGAL */}
+          <div className="flex flex-col sm:flex-row justify-between w-full sm:w-auto gap-6 flex-wrap">
 
-            {/* Socials */}
-            <div className="flex flex-col gap-2 w-1/2 min-w-[140px]">
+            {/* Social */}
+            <div className="flex flex-col gap-2 sm:w-1/2">
               <h4 className="text-[10px] font-black uppercase tracking-[0.4em] text-white/40 mb-2">
                 Social
               </h4>
@@ -46,7 +46,7 @@ export default function Footer() {
             </div>
 
             {/* Legal */}
-            <div className="flex flex-col gap-2 w-1/2 min-w-[140px]">
+            <div className="flex flex-col gap-2 sm:w-1/2">
               <h4 className="text-[10px] font-black uppercase tracking-[0.4em] text-white/40 mb-2">
                 Legal
               </h4>
